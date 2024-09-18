18ffdb6dab7ff8e9d885b496cf4d708b999a4f59
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-17 22:59:37 -0700

    Add todo and note models

--- main_app/models.py
+++ main_app/models.py
@@ -2,3 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    checked = models.BooleanField('', default=False)
+    text = models.CharField('', max_length=100)
+    def __str__(self):
+        return f'{self.text}, is complete: {self.checked}'
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField('')
+    def __str__(self):
+        return f'{self.title}: {self.body}'
